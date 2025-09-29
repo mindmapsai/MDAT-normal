a7db7b7f944d08963e8b818aa1f5cb6f996d74b5
Revanthmindmap <bathineni.revanthsai@mindmapconsulting.com>
2025-09-29 09:52:21 +0000

    version1.2

--- multi-department-system/src/services/api.js
+++ multi-department-system/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3002/api';
+const API_BASE_URL = `${process.env.REACT_APP_BACKEND_BASE_URL || 'http://localhost:3002'}/api`;
 
 class ApiService {
   constructor() {
